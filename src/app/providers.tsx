9c71e38ac6a1d1e0f9e8cb8bf2b4a4d9957a13dd
Pedro Henrique <pedro.lg.cs@gmail.com>
2024-10-15 11:08:30 -0300

    feat: configure next theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,22 @@
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from 'next-themes'
+import { Toaster } from 'sonner'
 
 import { queryClient } from '@/lib/react-query'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster position="top-right" richColors duration={3000} />
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
